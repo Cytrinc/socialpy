4048af8aeb9cdded1ee7bc10767d3d743249c1ab
QuirkWorx <158528051+quirkyworks@users.noreply.github.com>
2024-04-26 09:10:45 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# cytrinc social 0.0.2
+# this is tested in vscode
+
+print("welcome to cytrinc system")
+
+name = input("type a name: ")
+
+if name == "Mariocraft" or name == "Qwertyy":
+    print("Welcome back, " + name + "!")
+else:
+    print("Hello, " + name)
